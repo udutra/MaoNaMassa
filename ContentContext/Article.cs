d62b4a488507938caf9dae490b22340ed1492e3a
Guilherme Galarça Dutra <udutra@gmail.com>
2024-02-06 19:40:54 -0300

    Criando a classe de notificação

--- ContentContext/Article.cs
+++ ContentContext/Article.cs
@@ -1,7 +1,9 @@
-﻿namespace MaoNaMassa.ContentContext
+﻿using MaoNaMassa.NotificationsContext;
+
+namespace MaoNaMassa.ContentContext
 {
     public class Article(string title, string url) : Content(title, url)
     {
-
+        public IList<Notification> Notifications { get; set; }
     }
 }
---
+++ NotificationsContext/Notification.cs
@@ -0,0 +1,18 @@
+﻿namespace MaoNaMassa.NotificationsContext;
+
+public sealed class Notification
+{
+    public string Property { get; set; }
+    public string Message { get; set; }
+
+    public Notification()
+    {
+        
+    }
+    
+    public Notification(string property, string message)
+    {
+        Property = property;
+        Message = message;
+    }
+}
